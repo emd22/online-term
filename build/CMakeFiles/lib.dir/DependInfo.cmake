5b2405090888bd9f8444fb3f03149901f5864b6f
= <ethanmacdonald04@gmail.com>
2019-01-02 13:27:44 -0400

    trying to get simultanious users working

--- build/CMakeFiles/lib.dir/DependInfo.cmake
+++ build/CMakeFiles/lib.dir/DependInfo.cmake
@@ -4,19 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/ethan/Projects/online-term/src/lib/game.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/game.c.o"
-  "/home/ethan/Projects/online-term/src/lib/interface/keyboard.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/interface/keyboard.c.o"
-  "/home/ethan/Projects/online-term/src/lib/interface/ui.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/interface/ui.c.o"
-  "/home/ethan/Projects/online-term/src/lib/main.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/main.c.o"
-  "/home/ethan/Projects/online-term/src/lib/net/client.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/net/client.c.o"
-  "/home/ethan/Projects/online-term/src/lib/net/net.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/net/net.c.o"
-  "/home/ethan/Projects/online-term/src/lib/net/server.c" "/home/ethan/Projects/online-term/build/CMakeFiles/lib.dir/lib/net/server.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/game.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/game.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/interface/keyboard.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/interface/keyboard.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/interface/ui.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/interface/ui.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/main.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/main.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/net/client.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/net/client.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/net/net.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/net/net.c.o"
+  "/home/ethan/Desktop/online-term/src/lib/net/server.c" "/home/ethan/Desktop/online-term/build/CMakeFiles/lib.dir/lib/net/server.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  "/home/ethan/Projects/online-term/src/../include/lib"
+  "/home/ethan/Desktop/online-term/src/../include/lib"
   )
 
 # Targets to which this target links.
